bf63702719696cc20cee2a9edb27446ddf9c86ee
Mats Lian Krane <mats_krane@hotmail.com>
2022-03-08 14:35:48 +0100

    styled navbar xD

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,12 +4,23 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
     return (
-        <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/login'>Login</Link></li>
-            <li><Link to='/admin'>Admin</Link></li>
-        </ul>
+        <div className="navBar">
+            <div className="navLinks">
+                <ul>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/login'>Login</Link>
+                    </li>
+                    <li>
+                        <Link to='/admin'>Admin</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
     )
 }
+
 
 export default Nav;
