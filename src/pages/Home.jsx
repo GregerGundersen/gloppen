4faf247468582fda2c10e7b1bf2c1dafb69688d7
Siljesandersen <silje_sandersen@hotmail.com>
2022-03-10 13:40:56 +0100

    feat: rendered rooms

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { ROOM_URL } from "../utils/api";
 
 const Home = () => {
+  const [room, setRoom] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(ROOM_URL);
+      console.log(response);
+      setRoom(response.data.data)
+    }
+    fetchData().catch(console.error);
+  }, [])
+
   return (
     <div className="header">
       <h1>Home page</h1>
+      {room.map((room, idx) => {
+        return <div key={idx}><h2>{room.attributes.roomType}</h2><p>Beds: {room.attributes.numberOfBeds}</p><p>Room number: {room.attributes.roomNumber}</p></div>
+      })}
     </div>
   );
 };
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,7 @@
 export const BASE_URL = "https://gloppen.herokuapp.com/";
 export const AUTH_URL = `${BASE_URL}api/auth/local`;
 export const BOOKINGS_URL = `${BASE_URL}api/bookings`;
+export const ROOM_URL = `${BASE_URL}api/rooms`;
 
-export const AUTH_PATH = 'api/auth/local';
-export const BOOKINGS_PATH = 'api/bookings';
+export const AUTH_PATH = "api/auth/local";
+export const BOOKINGS_PATH = "api/bookings";
